3fc62cb9fdcdc265586f1dafb2d9ceb61cdb07d7
JQIANG125 <32261300+JQIANG125@users.noreply.github.com>
2018-11-04 08:22:26 +0800

    application

---
+++ application.py
@@ -0,0 +1,61 @@
+#import
+import json
+import nltk
+import string
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+result=[]
+judge=[]
+error=[]
+content=[]
+good_id=[]
+sid = SentimentIntensityAnalyzer()
+
+def score_translate(old_value,old_min,old_max,new_min,new_max):
+	OldRange=(old_max-old_min)  
+	NewRange=(new_max-new_min)  
+	NewValue = (((old_value-old_min)*NewRange)/OldRange)+new_min
+	return NewValue
+
+#data process
+with open('CellPhoneReview.json') as datafile:
+	for line in datafile:
+		data = json.loads(line)
+		text=data["reviewText"]
+		summary=data["summary"]
+		overall_score=data["overall"]
+		comment_id=data["reviewerID"]
+		goodid=data["asin"]
+		sens=nltk.sent_tokenize(text)
+		score=0
+		for sentence in sens:
+			ss=sid.polarity_scores(sentence)
+			score=score+ss["compound"]
+		sss=sid.polarity_scores(summary)
+		#allocate different weights to review text and summmary
+		score=0.2*score+0.8*sss["compound"]
+		if ((score>1 and overall_score<2.5) or (score<-1 and overall_score>2.5)):
+			score=score_translate(score,-3,3,1,5)
+			error.append(comment_id)
+			result.append(score)
+			judge.append(overall_score)
+			content.append(text)
+			good_id.append(goodid)
+			
+print("done")
+#show result
+print("There are " ,len(error), "comflix reviews")
+print()
+print()
+for x in range(len(error)):
+	print("reviewer id:",error[x])
+	print("product id",good_id[x])
+	print("judge result:",result[x])
+	print("overall score given by reviewer:",judge[x])
+	print("review text:",content[x])
+
+
+
+
+
